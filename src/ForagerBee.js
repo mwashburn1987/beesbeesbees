1436580a08538114698904b74c610f37106fe3b2
Michael Washburn <m.washburn@mail.com>
2022-02-28 10:46:16 -0800

    added code for all class/subclass instantiation, all tests passing

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function() {
+  //May need to create a treasure object, for now it's a string based on instructions
+  this.treasureChest.push('treasure');
 };
 
